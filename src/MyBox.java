1d3e7e6c154d2932940273819781a819ab84d805
jengjeng10 <asdasd@gm.com>
2025-07-23 17:52:24 +0900

    첫번째 연습

---
+++ src/MyBox.java
@@ -0,0 +1,19 @@
+public class MyBox {
+    public static void main(String [] args){
+
+        double width = 10;
+        double height = 5;
+        double area ;
+        double cir ;
+
+        area = width * height ;
+        cir = (width + height) * 2 ;
+
+        System.out.println("너비 : "+width);
+        System.out.println("높이 : "+height);
+        System.out.println("사각형의 넒이 : "+area);
+        System.out.println("사각형의 둘레 : "+cir);
+
+
+    }
+}
